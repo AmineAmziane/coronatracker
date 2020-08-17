89d41d4c3e41067813cafe53fe88d72859bf1a7f
AmineDz <amine2014max@gmail.com>
2020-08-17 21:17:29 +0100

    Updates

---
+++ precache-manifest.7c3c06ed9fca9dc6ab9386f2d6c042e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48ef79283abec4cebb563d09eff8b025",
+    "url": "/coronatracker/index.html"
+  },
+  {
+    "revision": "7b912827289303f19bcf",
+    "url": "/coronatracker/static/css/main.4acf774a.chunk.css"
+  },
+  {
+    "revision": "4207b00108303e9a0de4",
+    "url": "/coronatracker/static/js/2.8915fbc0.chunk.js"
+  },
+  {
+    "revision": "4db0b4b7fd2c7a0aaf4845dcd6ed7f11",
+    "url": "/coronatracker/static/js/2.8915fbc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b912827289303f19bcf",
+    "url": "/coronatracker/static/js/main.4bbd9fd1.chunk.js"
+  },
+  {
+    "revision": "d08c72dc70dbac9e3569",
+    "url": "/coronatracker/static/js/runtime-main.b4cd5e5e.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/coronatracker/static/media/logo.d7265326.png"
+  }
+]);
